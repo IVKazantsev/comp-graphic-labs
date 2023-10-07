6651e92d0cc58a12093879140ca6ea136b83ad0b
Ilya Kazancev <97786849+IVKazantsev@users.noreply.github.com>
2023-10-07 10:09:41 +0200

    Add files via upload

---
+++ BMViewer/resource.h
@@ -0,0 +1,55 @@
+//{{NO_DEPENDENCIES}}
+// Включаемый файл, созданный в Microsoft Visual C++.
+// Используется BMViewer.rc
+//
+#define IDD_ABOUTBOX                    100
+#define IDR_MAINFRAME                   128
+#define IDR_BMVIEWTYPE                  129
+#define IDD_HIST                        130
+#define IDD_BRIGHT_CONT                 131
+#define IDD_DENOISE                     132
+#define IDC_SLIDER_BRIGHTNESS           1001
+#define IDC_STATIC_BRIGHTNESS           1002
+#define IDC_SLIDER_CONTRAST             1003
+#define IDC_SLIDER_HIST_B               1003
+#define IDC_STATIC_CONTRAST             1004
+#define IDC_SLIDER_HIST_T               1004
+#define IDC_OFFSET_B_TXT                1005
+#define IDC_OFFSET_T_TXT                1006
+#define IDC_NOISE_SMOOTH                1008
+#define IDC_NOISE_SHOW                  1009
+#define IDC_K                           1011
+#define IDC_SPIN_K                      1012
+#define IDC_HIST_VIEW                   1014
+#define ID_VIEW_HISTOGRAM               32771
+#define ID_EDIT_HISTOGRAM               32772
+#define ID_EDIT_BRIGHTNESSANDCONTRAST   32773
+#define ID_EDIT_INVERTCOLORS            32774
+#define ID_EDIT_BLUR                    32775
+#define ID_EDIT_SHARP                   32776
+#define ID_EDIT_CONTOUR                 32777
+#define ID_EDIT_EDITHALF                32778
+#define ID_EDIT_HALF                    32779
+#define ID_EDIT_EMBOSS                  32780
+#define ID_EDIT_DENOISE                 32781
+#define ID_EDIT_STOP                    32782
+#define ID_VIEW_ZOOMIN                  32792
+#define ID_VIEW_ZOOMOUT                 32793
+#define ID_VIEW_STRETCHHALFTONE         32794
+#define ID_VIEW_STRETCHCOLORONCOLOR     32795
+#define ID_VIEW_BLACKONWHITE            32796
+#define ID_VIEW_STRETCHBLACKONWHITE     32797
+#define ID_VIEW_STRETCHWHITEONBLACK     32798
+#define ID_VIEW_STRETCHNWHITEONBLACK    32799
+
+// Next default values for new objects
+// 
+#ifdef APSTUDIO_INVOKED
+#ifndef APSTUDIO_READONLY_SYMBOLS
+#define _APS_3D_CONTROLS                     1
+#define _APS_NEXT_RESOURCE_VALUE        133
+#define _APS_NEXT_COMMAND_VALUE         32800
+#define _APS_NEXT_CONTROL_VALUE         1015
+#define _APS_NEXT_SYMED_VALUE           101
+#endif
+#endif
