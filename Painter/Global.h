a098910b49a56eac074e45f2b2b6254dbb778712
Ilya Kazancev <97786849+IVKazantsev@users.noreply.github.com>
2023-10-07 10:04:40 +0200

    Add files via upload

---
+++ Painter/Global.h
@@ -0,0 +1,5 @@
+// ���� Global.cpp
+////////////////////////////////////////
+// ��������� ���������� �������
+CPoint Transform(const CPoint &point, const CPoint &point0, double ang, int a, int b);
+double ZFunction(double x, double y);
